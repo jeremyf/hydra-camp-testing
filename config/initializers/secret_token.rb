83d0b1bd12c42d50f49b27b32a9592aab41f18f5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-26 20:20:28 -0400

    Initializing Rails application
    NOTE: I am explicitly saying to not include test-unit. More on that in
    a bit.
    
    The following commands were run:
    $ rails new hydra-camp-devise-testing --skip-test-unit
    $ cd hydra-camp-devise-testing
    $ git init
    $ git add .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HydraCampDeviseTesting::Application.config.secret_key_base = '85a3787f040f954a753adadf8daad12d025bd4a6f938c0ac3c920f6029a3aab7d871e0f6393e50747aa3ed2cc08a2964b1e3b73d88c9148975c7a95925f3573e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HydraCampDeviseTesting::Application.config.session_store :cookie_store, key: '_hydra-camp-devise-testing_session'
