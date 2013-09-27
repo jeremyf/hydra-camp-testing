3331d9d9ae5e11045507f9d729412bd1a5ce76ce
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-27 17:39:07 -0400

    Adding the first thoughts on user signup
    The describe do, it do is the foundational structure of rspec.
    "Describe" provides the context in which we are running our specs.
    "It" is the context for an expectation/assertion.

---
+++ spec/features/user_signup_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'user signup' do
+  it 'attempting to login without an account' do
+    # Given I don't have an account
+    # When I go to the user sign in page
+    # And fill in my email and password
+    # And click Sign in
+    # Then I should see an error saying I am not registered
+  end
+end
