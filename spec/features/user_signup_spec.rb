9e9436af37a92676eb331b5a468fcc750e6f2018
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-28 08:21:59 -0400

    Adding second spec step for user sign up
    Filling out the sign in form, I needed to see what the page looked
    like, in particular what the DOM elements were.
    
    So I fired up the `rails server` (`rails s` is the synonym) and
    visited http://localhost:3000/users/sign_in. With my developer tools
    (Firebug or Developer Tools) I inspected the DOM and found the
    `form.edit_user` CSS selector to be reasonable.
    
    More information about targeting elements via Capybara:
    
    https://github.com/jnicklas/capybara#scoping

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -4,8 +4,11 @@ describe 'user signup' do
   it 'attempting to login without an account' do
     # Given I don't have an account
     visit('/users/sign_in')
-    # And fill in my email and password
-    # And click Sign in
+    within('form.new_user') do
+      fill_in('Email', with: 'hello@test.com')
+      fill_in('Password', with: 'myrealpassword')
+      click_button("Sign in")
+    end
     # Then I should see an error saying I am not registered
   end
 end
