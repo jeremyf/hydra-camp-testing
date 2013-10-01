452e2108360b7accc70f0ccafc71d6c9a46aea63
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-10-01 15:51:18 -0400

    Adding user creation spec
    A simple test against what Devise provides in the User model. While
    this is a somewhat contrived test, you should always test your
    application's interaction with underlying gems.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should allow users to be created' do
+    # Given I don't have any users
+    User.count.should == 0
+
+    # When I create a user
+    User.create!(email: 'jeremy.n.friesen@gmail.com', password: 'password')
+
+    # Then the User.count should be 1
+    User.count.should == 1
+  end
+
 end
